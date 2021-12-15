ae1c014557a9bb340cff3dac79155438ebf169cb
Ross Keenan <rossk29@gmail.com>
2021-12-15 17:47:37 +0200

    style: :lipstick: Settings tab styling

--- src/SettingTab.ts
+++ src/SettingTab.ts
@@ -12,27 +12,30 @@ export class SettingTab extends PluginSettingTab {
   display(): void {
     let { containerEl, plugin } = this;
     containerEl.empty();
+    containerEl.classList.add('emails-settings')
     const { settings } = this.plugin;
 
     const templateDesc = new DocumentFragment();
-    templateDesc.createDiv().innerHTML = `The template to use when creating a new file.<br/>
-    <strong>{{attachments}}</strong> → list of attachments<br/>
+    const templateDescDiv = templateDesc.createDiv();
+    templateDescDiv.classList.add("template-description");
+    templateDescDiv.innerHTML = `The template to use when creating a new file.<br/>
+    <code>{{attachments}}</code>: list of attachments<br/>
 
-    <strong>{{to}}</strong> → list of recipients (name + email)<br/>
+    <code>{{to}}</code>: list of recipients (name + email)<br/>
     <ul>
-    <li><strong>{{to.name}}</strong> → list of recipient names<br/></li>
-    <li><strong>{{to.email}}</strong> → list of recipient emails<br/></li>
+    <li><code>{{to.name}}</code>: list of recipient names<br/></li>
+    <li><code>{{to.email}}</code>: list of recipient emails<br/></li>
     </ul>
     <br/>
-    <strong>{{from}}</strong> → sender (name + email)<br/>
+    <code>{{from}}</code>: sender (name + email)<br/>
     <ul>
-    <li><strong>{{from.name}}</strong> → sender name<br/></li>
-    <li><strong>{{from.email}}</strong> → sender email<br/></li>
+    <li><code>{{from.name}}</code>: sender name<br/></li>
+    <li><code>{{from.email}}</code>: sender email<br/></li>
     </ul>
     <br/>
-    <strong>{{subject}}</strong> → subject<br/>
-    <strong>{{text}}</strong> → text body<br/>
-    <strong>{{date}}</strong> → date`;
+    <code>{{subject}}</code>: subject<br/>
+    <code>{{text}}</code>: text body<br/>
+    <code>{{date}}</code>: date`;
 
     new Setting(containerEl)
       .setName("Template")
